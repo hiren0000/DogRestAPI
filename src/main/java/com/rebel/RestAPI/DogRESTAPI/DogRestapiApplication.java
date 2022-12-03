17ae6c61899b82162d4bbc0ca7c4a56031125efb
Hiren <rebeldevmurari@gmail.com>
2022-12-03 20:10:25 +0000

    Initial commit

---
+++ src/main/java/com/rebel/RestAPI/DogRESTAPI/DogRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.rebel.RestAPI.DogRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebel/RestAPI/DogRESTAPI/DogRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.RestAPI.DogRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
